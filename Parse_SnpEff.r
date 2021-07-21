e288a53202038af9e6fe1a0c85a11eacca80d6d1
Haogao <koohoko@gmail.com>
2021-07-21 17:10:55 +0800

    change amino acid codes to abbreviations

--- Parse_SnpEff.r
+++ Parse_SnpEff.r
@@ -11,7 +11,31 @@ library(tidyverse)
 # input_vcf <- "../data/delta_hk.snpeff.vcf"
 input_vcf <- args[1]
 
-df_vcf_ann <- read_tsv(input_vcf, comment = "#", col_names = F)
+file.copy(input_vcf, "./tmp.vcf")
+# the following works for MacOS
+system("sed -i ' ' 's/Ala/A/'g tmp.vcf
+sed -i ' ' 's/Arg/R/'g tmp.vcf
+sed -i ' ' 's/Asn/N/'g tmp.vcf
+sed -i ' ' 's/Asp/D/'g tmp.vcf
+sed -i ' ' 's/Cys/C/'g tmp.vcf
+sed -i ' ' 's/Glu/E/'g tmp.vcf
+sed -i ' ' 's/Gln/Q/'g tmp.vcf
+sed -i ' ' 's/Gly/G/'g tmp.vcf
+sed -i ' ' 's/His/H/'g tmp.vcf
+sed -i ' ' 's/His/H/'g tmp.vcf
+sed -i ' ' 's/Ile/I/'g tmp.vcf
+sed -i ' ' 's/Leu/L/'g tmp.vcf
+sed -i ' ' 's/Lys/K/'g tmp.vcf
+sed -i ' ' 's/Met/M/'g tmp.vcf
+sed -i ' ' 's/Phe/F/'g tmp.vcf
+sed -i ' ' 's/Pro/P/'g tmp.vcf
+sed -i ' ' 's/Ser/S/'g tmp.vcf
+sed -i ' ' 's/Thr/T/'g tmp.vcf
+sed -i ' ' 's/Trp/W/'g tmp.vcf
+sed -i ' ' 's/Tyr/Y/'g tmp.vcf
+sed -i ' ' 's/Val/V/'g tmp.vcf")
+
+df_vcf_ann <- read_tsv("./tmp.vcf", comment = "#", col_names = F)
 df_vcf_ann$mut <- sapply(df_vcf_ann$X8, function(x){
 	strsplit(x, ";")[[1]][1]
 }, USE.NAMES = F)
@@ -51,3 +75,5 @@ df_vcf_ann$pos_aa <- sapply(df_vcf_ann$X8, function(x){
 df_vcf_ann$in_gene <- df_vcf_ann$effect %in% c("missense_variant", "synonymous_variant")
 
 df_vcf_ann %>% select(-X3, -X6, -X7, -X8) %>% write_csv(args[2])
+file.remove("./tmp.vcf")
+file.remove("./tmp.vcf ")
