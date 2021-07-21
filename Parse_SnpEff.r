890c5ab2661f1ca728720a448ec388fd0bb441e5
Haogao <koohoko@gmail.com>
2021-07-21 14:38:07 +0800

    parsing script

---
+++ Parse_SnpEff.r
@@ -0,0 +1,51 @@
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly=TRUE)
+if (length(args)==0) {
+  stop("At least one argument must be supplied (input file).n", call.=FALSE)
+} else if (length(args)==1) {
+  # default output file
+  args[2] = "out.csv"
+}
+library(tidyverse)
+
+# input_vcf <- "../data/delta_hk.snpeff.vcf"
+input_vcf <- args[1]
+
+df_vcf_ann <- read_tsv(input_vcf, comment = "#", col_names = F)
+df_vcf_ann$mut <- sapply(df_vcf_ann$X8, function(x){
+	strsplit(x, ";")[[1]][1]
+}, USE.NAMES = F)
+df_vcf_ann$effect <- sapply(df_vcf_ann$X8, function(x){
+	tmp <- strsplit(x, ";")[[1]][2] # only consider the nearest match
+	strsplit(tmp, "|", fixed = T)[[1]][2]
+}, USE.NAMES = F)
+df_vcf_ann$impact <- sapply(df_vcf_ann$X8, function(x){
+	tmp <- strsplit(x, ";")[[1]][2]
+	strsplit(tmp, "|", fixed = T)[[1]][3]
+}, USE.NAMES = F)
+df_vcf_ann$gene <- sapply(df_vcf_ann$X8, function(x){
+	tmp <- strsplit(x, ";")[[1]][2]
+	strsplit(tmp, "|", fixed = T)[[1]][4]
+}, USE.NAMES = F)
+df_vcf_ann$gene_name <- sapply(df_vcf_ann$X8, function(x){
+	tmp <- strsplit(x, ";")[[1]][2]
+	strsplit(tmp, "|", fixed = T)[[1]][5]
+}, USE.NAMES = F)
+df_vcf_ann$mut_cdna <- sapply(df_vcf_ann$X8, function(x){
+	tmp <- strsplit(x, ";")[[1]][2]
+	strsplit(tmp, "|", fixed = T)[[1]][10]
+}, USE.NAMES = F)
+df_vcf_ann$mut_aa <- sapply(df_vcf_ann$X8, function(x){
+	tmp <- strsplit(x, ";")[[1]][2]
+	strsplit(tmp, "|", fixed = T)[[1]][11]
+}, USE.NAMES = F)
+df_vcf_ann$pos_cdna <- sapply(df_vcf_ann$X8, function(x){
+	tmp <- strsplit(x, ";")[[1]][2]
+	strsplit(tmp, "|", fixed = T)[[1]][12]
+}, USE.NAMES = F)
+df_vcf_ann$pos_aa <- sapply(df_vcf_ann$X8, function(x){
+	tmp <- strsplit(x, ";")[[1]][2]
+	strsplit(tmp, "|", fixed = T)[[1]][14]
+}, USE.NAMES = F)
+
+df_vcf_ann %>% select(-X3, -X6, -X7, -X8) %>% write_csv(args[2])
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # Parsing-SnpEff
+
+We use [SnpEff](https://pcingola.github.io/SnpEff/se_inputoutput/#ann-field-vcf-output-files) to annotate vcf files, then parse the results to regular tables with R scripts.
+
+## Install and download database
+Download SnpEff via this [link](https://pcingola.github.io/SnpEff/download/).
+Download SARS-CoV-2 database via: 
+``` sh
+java -jar ~/softwares/snpEff/snpEff.jar download -c ~/softwares/snpEff/snpEff.config -v MN908947.3`
+```
+
+## Annotate
+Run SnpEff as:
+``` sh
+# run snpeff annotation on vcf
+java -jar ~/softwares/snpEff/snpEff.jar MN908947.3 -fastaProt ../results/example.snpeff.vcf.faa -csvStats ../results/example.snpeff.vcf.stats ../results/example.vcf > ../results/example.snpeff.vcf
+# run bcftools csq to link consecutive SNPs on the same codon (BCSQ field)
+bcftools csq --force --phase a -f reference.fasta -g ../data/genes.gff -Ov variants.snpeff.vcf -o variants.snpeff.csq.vcf
+
+```
+
+## Parse the results
+Run the R scripts:
+``` sh
+Rscript ./Parse_SnpEff.r ../results/example.snpeff.vcf ../results/example.snpeff.csv
+```
